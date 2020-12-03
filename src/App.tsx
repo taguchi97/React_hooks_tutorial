067749802ea13f41393b25d38c73f4cbdd3b5e3a
taguchi97 <a.taguchi97@gmail.com>
2020-12-03 22:55:36 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import "./App.css"
+import { BookToRead } from "./BookToRead"
+import BookRow from './BookRow'
+import BookSearchItem from './BookSearchItem';
+import { BookDescription } from './BookDescription';
+import BookSearchDialog from './BookSearchDialog';
+import Modal from 'react-modal'
 
-function App() {
+Modal.setAppElement('#root')
+
+const customStyles = {
+  // overlay: {
+  //   backgroundcolor: 'rgba(0,0,0,0.8)'
+  // },
+  content: {
+    top: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    left: '50%',
+    marginRight: '-50%',
+    padding: 0,
+    transform: 'translate(-50%, -50%)'
+  }
+}
+const APP_KEY = 'react-hooks-tutorial'
+
+const App = () => {
+  const [books, setBooks] = useState<BookToRead[]>([])
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false)
+  useEffect(()=>{
+    const storeBooks = localStorage.getItem(APP_KEY)
+    if (storeBooks) {
+      setBooks(JSON.parse(storeBooks))
+    }
+  }, [])
+    useEffect(()=>{
+    localStorage.setItem(APP_KEY, JSON.stringify(books))
+  },[books])
+
+  const handleAddClick = () => {
+    setModalIsOpen(true)
+  }
+  const handleModalClose = () => {
+    setModalIsOpen(false)
+  }
+  const handleBookDelete = (id: number) => {
+    const newBooks = books.filter((b) => b.id !== id)
+    setBooks(newBooks)
+  }
+  const handleBookMemoChange = (id: number, memo: string) => {
+    const newBooks = books.map((b) => {
+      return b.id === id
+        ? { ...b, memo: memo }
+        : b
+    })
+    setBooks(newBooks)
+  }
+  const onBookAdd = (b: BookDescription) => {
+    const id = books.length + 1
+    const newBook: BookToRead = {
+      id: id,
+      title: b.title,
+      authors: b.authors,
+      memo: ""
+    }
+    const newBooks: BookToRead[] = books.concat(newBook)
+    setBooks(newBooks)
+    console.log(newBooks)
+  }
+  const BookRows = books.map((b) => {
+    return (
+      <BookRow
+        book={b}
+        key={b.id}
+        onMemoChange={handleBookMemoChange}
+        onDelete={handleBookDelete}
+      />
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="nav">
+        <h1>読みたい本リスト</h1>
+        <div className="button-like" onClick={handleAddClick}>本を追加</div>
+      </section>
+      <section className="main">
+        {BookRows}
+      </section>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleModalClose}
+        style={customStyles}
+      >
+        <BookSearchDialog onBookAdd={onBookAdd} maxResults={20}/> 
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/BookSearchDialog.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react'
+import { BookDescription } from './BookDescription'
+import BookSearchItem from './BookSearchItem'
+
+function buildSearchUrl(title: string, author: string, maxResults: number): string {
+  let url = "https://www.googleapis.com/books/v1/volumes?q=";
+  const conditions: string[] = []
+  if (title) {
+    conditions.push(`intitle:${title}`);
+  }
+  if (author) {
+    conditions.push(`inauthor:${author}`);
+  }
+  return url + conditions.join('+') + `&maxResults=${maxResults}`;
+}
+
+function extractBooks(json: any): BookDescription[] {
+  const items: any[] = json.items;
+  return items.map((item: any) => {
+    const volumeInfo: any = item.volumeInfo;
+    return {
+      title: volumeInfo.title,
+      authors: volumeInfo.authors ? volumeInfo.authors.join(', ') : "",
+      thumbnail: volumeInfo.imageLinks ? volumeInfo.imageLinks.smallThumbnail : "",
+    }
+  });
+}
+
+type BookSearchDialogProps = {
+  maxResults : number
+  onBookAdd: (b: BookDescription) => void
+}
+
+const BookSearchDialog = (props: BookSearchDialogProps) => {
+  const [books, setBooks] = useState<BookDescription[]>([])
+  const [title, setTitle] = useState<String>('')
+  const [author, setAuthor] = useState<String>('')
+  const [isSearching, setIsSearching] = useState<boolean>(false)
+  useEffect(() =>{
+    if(isSearching) {
+      const url = buildSearchUrl(title, author, props.maxResults)
+      fetch(url)
+        .then((res)=>{
+          return res.json()
+        })
+        .then((json)=>{
+          return extractBooks(json)
+        })
+        .then((books)=>{
+          setBooks(books)
+        })
+        .catch((err)=>{
+          console.error(err)
+        })
+    }
+    setIsSearching(false)
+  },[isSearching])
+
+  const handleTitleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  }
+  const handleAuthorInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAuthor(e.target.value)
+  }
+  const handleSearchClick = () => {
+    if (!title && !author) {
+      alert('検索条件を入力してください')
+      return
+    }
+    setIsSearching(true)
+    //検索を実行
+  }
+  const handleBookAdd = (b: BookDescription) => {
+    props.onBookAdd(b)
+  }
+  const bookItems = books.map((b: BookDescription, idx: number) => {
+    return (
+      <BookSearchItem
+        description={b}
+        onBookAdd={(b) => { handleBookAdd(b) }}
+        key={idx}
+      />
+    )
+  })
+  return (
+    <div className="dialog">
+      <div className="operation">
+        <div className="conditions">
+          <input
+            type="text"
+            value={title}
+            onChange={handleTitleInputChange}
+            placeholder="タイトルで検索"
+          />
+          <input
+            type="text"
+            value={author}
+            onChange={handleAuthorInputChange}
+            placeholder="著者名で検索"
+          />
+        </div>
+        <div className="button-like" onClick={handleSearchClick}>
+          検索
+        </div>
+      </div>
+      <div className="search-results">{bookItems}</div>
+    </div>
+  )
+}
+
+export default BookSearchDialog
